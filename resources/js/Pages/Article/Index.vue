2dbb0a334423bc39d4a1850c22c1983bd8b9b3be
Markus <lebrabusiness@gmail.com>
2024-04-16 10:14:41 +0300

    posts styling on index page

--- resources/js/Pages/Article/Index.vue
+++ resources/js/Pages/Article/Index.vue
@@ -2,7 +2,6 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import { Link } from '@inertiajs/vue3';
-import { Head } from '@inertiajs/vue3';
 
 const props = defineProps(['articles'])
 </script>
@@ -15,20 +14,26 @@ const props = defineProps(['articles'])
         <template #header>
             <PrimaryButton class="">
                     <Link :href="route('articles.create')">Create new Article</Link>
-                </PrimaryButton>
+            </PrimaryButton>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
-                    <div class="p-2 text-gray-900 flex flex-col justify-between border-b" v-for="article in articles">
-                        <div class="">
-                            <h1 class="text-2xl font-bold">{{ article.title }}</h1>
-                            <h1 class="text-base">{{ article.description }}</h1>
+                <div class=" overflow-hidden shadow-sm sm:rounded-lg p-6 grid grid-cols-2">
+                    <div class="p-2 text-gray-900" v-for="article in articles">
+                        <div class="border-2 relative flex border-gray rounded-lg min-h-64 p-4">
+                            <div>
+                                <h1 class="text-2xl border-b mb-1 font-bold">{{ article.title }}</h1>
+                                <h1 class="text-base">{{ article.description }}</h1>
+                            </div>
+
+                            <div class="absolute pr-2 pb-1 text-emerald-500 right-0 bottom-0">
+                                <Link class="" :href="route('articles.edit', {article})">Edit</Link>
+                            </div>
                         </div>
+                        
 
                         <div class="flex gap-4">
-                            <Link :href="route('articles.edit', {article})">Edit</Link>
                             <!-- <Link class="text-red-500" :href="route('articles.destroy', {articles})" method="delete" as="button" type="button">Delete</Link> -->
                         </div>
                         <!-- <div class="flex my-12 flex-col">
